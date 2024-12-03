cd201eb0b3960778f310ad52100baf23c51b8ccb
Lareb Noor <156116687+noor2607@users.noreply.github.com>
2024-12-03 12:00:58 -0800

    updated eslint

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -6,23 +6,20 @@ export default function About() {
           <div className="abouttext">
             <h1>A Glimpse Into My World</h1>
             <p>
-              <p>
-                Hello! Iam Lareb Noor, a passionate web developer with a deep
-                love for building functional and beautiful digital experiences. My
-                journey into the world of coding began 1 years ago when I
-                realized how technology could turn creative ideas into reality.
-                Since then, Ive been honing my skills in web development,
-                specializing in both front-end and back-end technologies such as
-                HTML, CSS, JavaScript, Typescript figma github React,
-                Node.js, Next.js. My goal is to always build websites that are not
-                only visually appealing but also user-friendly and accessible to
-                everyone. I love solving problems and embracing challenges, which
-                is why Iam constantly learning and experimenting with new
-                technologies and tools.
-                Iam always looking for
-                exciting projects to work on, so feel free to reach out if you
-                think we would be a great fit!
-              </p>
+              Hello! I am Lareb Noor, a passionate web developer with a deep
+              love for building functional and beautiful digital experiences. My
+              journey into the world of coding began 1 year ago when I
+              realized how technology could turn creative ideas into reality.
+              Since then, I&apos;ve been honing my skills in web development,
+              specializing in both front-end and back-end technologies such as
+              HTML, CSS, JavaScript, Typescript, Figma, GitHub, React,
+              Node.js, and Next.js. My goal is to always build websites that are not
+              only visually appealing but also user-friendly and accessible to
+              everyone. I love solving problems and embracing challenges, which
+              is why I am constantly learning and experimenting with new
+              technologies and tools. I am always looking for
+              exciting projects to work on, so feel free to reach out if you
+              think we would be a great fit!
             </p>
           </div>
         </div>
